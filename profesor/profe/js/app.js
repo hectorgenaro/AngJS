d96d8ab38fc0cd2aef9d27676da8e027391fd2cc
hectorgenaro <hectorgenaro@gmail.com>
2019-11-11 23:21:18 -0600

    agregando funcion para el boton editar en el archivo profesor angularJs

--- profesor/profe/js/app.js
+++ profesor/profe/js/app.js
@@ -1,28 +1,37 @@
-var app= angular.module('universidaApp', [ ])
-app.controller('profesorCtrl', function($scope){
-  $scope.profesor= profesorData;
-  $scope.editando= {};
-  $scope.mostrarCaja= false;
+var app = angular.module('universidaApp', [])
+app.controller('profesorCtrl', function($scope) {
+  $scope.profesor = profesorData;
+  $scope.editando = {};
+  $scope.mostrarCaja = false;
+  $scope.contador = 0;
 
-  $scope.editarProfesor= function () {
+
+  $scope.editarProfesor = function() {
     angular.copy($scope.profesor, $scope.editando)
-    $scope.mostrarCaja= true;
+    $scope.mostrarCaja = true;
+    $scope.contador = $scope.contador + 1;
+    console.log($scope.contador)
+    if ($scope.contador % 2 === 0 && $scope.mostrarCaja == true ) {
+      $scope.mostrarCaja = false;
+       console.log($scope.mostrarCaja)
+    } 
   }
 
-  $scope.guardarCambios= function(){
+
+  $scope.guardarCambios = function() {
     angular.copy($scope.editando, $scope.profesor)
-    $scope.mostrarCaja= false;
+    $scope.mostrarCaja = false;
   }
 
-  $scope.cancelarCambios= function(){
-    $scope.editando={};
-    $scope.mostrarCaja= false;
+  $scope.cancelarCambios = function() {
+    $scope.editando = {};
+    $scope.mostrarCaja = false;
   }
 });
 
-  var profesorData = {
-	nombre: "Juan Carlos Pineda",
-	bio: "Saludos estudiante, mi nombre es Juan Carlos, encantado de conocerte, soy una apasionado instructor de matemáticas aplicadas cuánticas, más orientado a la física termonuclear. Mi vocación es ser maestro y lograr transmitir mis conocimientos a todos mis estudiantes!.",
-	edad: 47,
-	foto: "img/juancarlos.jpg"
+var profesorData = {
+  nombre: "Juan Carlos Pineda",
+  bio: "Saludos estudiante, mi nombre es Juan Carlos, encantado de conocerte, soy una apasionado instructor de matemáticas aplicadas cuánticas, más orientado a la física termonuclear. Mi vocación es ser maestro y lograr transmitir mis conocimientos a todos mis estudiantes!.",
+  edad: 47,
+  foto: "img/juancarlos.jpg"
 }

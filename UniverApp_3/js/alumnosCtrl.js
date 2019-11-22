6a3c608d4985dd8c26c984513c69533b7509f5d2
hectorgenaro <hectorgenaro@gmail.com>
2019-11-22 02:26:59 -0600

    agregando nueva carpeta udemy

---
+++ UniverApp_3/js/alumnosCtrl.js
@@ -0,0 +1,9 @@
+app.controller('alumnosCtrl', ['$scope','$http', function($scope,$http){
+
+	$scope.setActive("mAlumnos");
+	$scope.alumnos = {};
+	$http.get('php/servicios/alumnos.listado.php').success(function(data){
+		$scope.alumnos = data;
+	})
+
+}]);

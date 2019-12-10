9358a3581f0a1ba4e00c5a9f916a89732ef70afe
hectorgenaro <hectorgenaro@gmail.com>
2019-12-10 01:28:12 -0600

    add servicio que actualiza info de alumno

--- UniverApp_3/js/alumnoCtrl.js
+++ UniverApp_3/js/alumnoCtrl.js
@@ -1,6 +1,7 @@
 app.controller('alumnoCtrl', ['$scope','$routeParams', '$http', function($scope, $routeParams, $http){
 	$scope.setActive("mAlumnos");
 var codigo = $routeParams.codigo;
+$scope.actualizado = false;
 $scope.alumno = {};
 $http.get("php/servicios/alumnos.getAlumno.php?c=" + codigo).success(function(data){
   if(data.err != undefined){
@@ -9,4 +10,15 @@ $http.get("php/servicios/alumnos.getAlumno.php?c=" + codigo).success(function(da
   }
   $scope.alumno = data;
 })
+	$scope.guardarAlumno = function(){
+		$http.post('php/servicios/alumnos.guardar.php', $scope.alumno).success(function(data){
+			if (data.err === false){
+				$scope.actualizado = true;
+				setTimeout(function () {
+					$scope.actualizado = false;
+					$scope.$apply()
+				}, 2000);
+			}
+		})
+	}
 }]);

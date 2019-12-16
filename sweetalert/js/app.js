4c129b6b94c1bd0c03aa68b6d4821c47833e7ca7
hectorgenaro <hectorgenaro@gmail.com>
2019-12-15 23:34:38 -0600

    add btns con diferentes funciones

--- sweetalert/js/app.js
+++ sweetalert/js/app.js
@@ -3,10 +3,36 @@ var app = angular.module('bonusApp',[ ]);
 
 app.controller('mainCtrl', ['$scope', function($scope){
 $scope.mensaje = "Hola mundo!!"
+$scope.mensaje_titulo = "One line"
+$scope.mensaje_subtitulo = "This is another line"
 $scope.mostrar_alerta = function(){
 	Swal.fire($scope.mensaje);
 }
-
-
+$scope.mostrar_subtitulo = function(){
+	Swal.fire(
+  $scope.mensaje_titulo,
+  $scope.mensaje_subtitulo,
+  'question'
+)
+}
+$scope.mostrar_confirmacion = function(){
+	Swal.fire({
+	  title: 'Are you sure?',
+	  text: "You won't be able to revert this!",
+	  icon: 'warning',
+	  showCancelButton: true,
+	  confirmButtonColor: '#3085d6',
+	  cancelButtonColor: '#d33',
+	  confirmButtonText: 'Yes, delete it!'
+	}).then((result) => {
+	  if (result.value) {
+	    Swal.fire(
+	      'Deleted!',
+	      'Your file has been deleted.',
+	      'success'
+	    )
+	  }
+	})
+}
 
 }]);

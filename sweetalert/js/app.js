d90bb2168fbbde7195f6d9bde625be291df9b919
hectorgenaro <hectorgenaro@gmail.com>
2019-12-15 22:44:21 -0600

    add cdn y alert test

--- sweetalert/js/app.js
+++ sweetalert/js/app.js
@@ -2,9 +2,11 @@
 var app = angular.module('bonusApp',[ ]);
 
 app.controller('mainCtrl', ['$scope', function($scope){
+$scope.mensaje = "Hola mundo!!"
+$scope.mostrar_alerta = function(){
+	Swal.fire($scope.mensaje);
+}
 
 
 
-
-	
 }]);

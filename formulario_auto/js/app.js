cacb94e2fa6ea7540a23374f994a4ebaa02f6c82
hectorgenaro <hectorgenaro@gmail.com>
2019-12-17 02:02:49 -0600

    add validaciones automaticas libreria autovalidate

--- formulario_auto/js/app.js
+++ formulario_auto/js/app.js
@@ -1,24 +1,28 @@
 
-var app = angular.module('bonusApp',[ ]);
+var app = angular.module('bonusApp',['jcs-autoValidate' ]);
 
 app.controller('mainCtrl', ['$scope', function($scope){
 
+	angular.module('bonusApp')
+			.run([
+			'defaultErrorMessageResolver',
+			function (defaultErrorMessageResolver) {
+					// passing a culture into getErrorMessages('fr-fr') will get the culture specific messages
+					// otherwise the current default culture is returned.
+					defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+						errorMessages['edadMinima'] = 'Debe ser mayor a {0} años';
+						errorMessages['edadMaxima'] = 'Debe ser menor a {0} años';
+					});
+			}
+	]);
 	$scope.formData = {};
 
 
-	$scope.guardar_datos = function( valido ){
-
-		if( !valido ){
-			return;
+	$scope.guardar_datos = function(  ){
+		console.log("Posteando...");
 		}
 
 
 
-		console.log("Posteando...");
 
-
-	}
-
-
-	
 }]);

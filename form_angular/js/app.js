19d3594f19d798c58b78c4be6bc17c2f09bbdba6
hectorgenaro <hectorgenaro@gmail.com>
2019-12-16 22:39:48 -0600

    agragando validacion funcion guardando

--- form_angular/js/app.js
+++ form_angular/js/app.js
@@ -3,8 +3,9 @@ var app = angular.module('bonusApp',[ ]);
 
 app.controller('mainCtrl', ['$scope', function($scope){
 $scope.form = {};
-$scope.guardar = function () {
-	console.log("guardando...")
+$scope.guardar = function (valido) {
+	if (!valido ) return
+	console.log ("guardando")
 }
 
 

fab3bb42160ec4dc1d814dfc2da87e0dcffdcae2
hectorgenaro <hectorgenaro@gmail.com>
2020-01-10 01:31:25 -0600

    seteando la variable en error

--- promesas/angular/controladores.js
+++ promesas/angular/controladores.js
@@ -2,29 +2,31 @@ var app = angular.module('promesaApp.controladores', [])
 app.controller('mainCtrl', ['$scope','$q', function($scope, $q){
 
   $scope.miVar = 0;
-  console.log("todo ok")
-
   $scope.sumar= function (num){
     var q = $q.defer();
-    var valido = true;
+    var valido = false;
     num++;
-    if(valido){
-      q.resolve(num)
-    }else{
-      q.reject('no se sumar')
-    }
+    setTimeout(function () {
+      if(valido){
+        q.resolve(num)
+      }else{
+        q.reject('no se sumar')
+      }
+    }, 2000);
+
     return q.promise;
   }
 
   $scope.promise = $scope.sumar(1);
   $scope.promise.then (
     function (valor){
-      console.log("promesa cumplida")
+      console.log("Promesa cumplida")
       $scope.miVar= valor;
     },
-    function() {
+    function(error) {
       console.error(error);
-  })
+      $scope.miVar= "Error!!!";
+})
 
 
 }])

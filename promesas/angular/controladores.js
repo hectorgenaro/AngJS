00f13a2cec77371485648b3272d18e6f8211b027
hectorgenaro <hectorgenaro@gmail.com>
2020-01-10 01:07:12 -0600

    add objeto $q para ejecutar promesa

--- promesas/angular/controladores.js
+++ promesas/angular/controladores.js
@@ -1,7 +1,30 @@
 var app = angular.module('promesaApp.controladores', [])
-app.controller('mainCtrl', ['$scope', function($scope){
+app.controller('mainCtrl', ['$scope','$q', function($scope, $q){
 
   $scope.miVar = 0;
   console.log("todo ok")
+
+  $scope.sumar= function (num){
+    var q = $q.defer();
+    var valido = true;
+    num++;
+    if(valido){
+      q.resolve(num)
+    }else{
+      q.reject('no se sumar')
+    }
+    return q.promise;
+  }
+
+  $scope.promise = $scope.sumar(1);
+  $scope.promise.then (
+    function (valor){
+      console.log("promesa cumplida")
+      $scope.miVar= valor;
+    },
+    function() {
+      console.error(error);
+  })
+
 
 }])

78c14fa4055645986eb9e831d4dc3a32b686ebbc
hectorgenaro <hectorgenaro@gmail.com>
2020-01-16 02:46:13 -0600

    eliminando console.log

--- servicios/angular/servicios.js
+++ servicios/angular/servicios.js
@@ -9,7 +9,6 @@ app.factory('Personas', ['$http', '$q', function ($http, $q){
   self.cargarData = function(){
     self.cargando = true;
     var q = $q.defer();
-    console.log("funcion llamada")
     $http.jsonp("http://www.json-generator.com/api/json/get/cflPmSUZMy?callback=JSON_CALLBACK")
       .then(function success(data){
         console.log ("todo bien")

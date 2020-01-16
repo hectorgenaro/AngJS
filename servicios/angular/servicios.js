8fcdbb41c328055c23408a09a0acbcd1d514936b
hectorgenaro <hectorgenaro@gmail.com>
2020-01-16 02:26:01 -0600

    respuesta de api json

--- servicios/angular/servicios.js
+++ servicios/angular/servicios.js
@@ -10,7 +10,7 @@ app.factory('Personas', ['$http', '$q', function ($http, $q){
     self.cargando = true;
     var q = $q.defer();
     console.log("funcion llamada")
-    $http.jsonp("http://www.json-generator.com/api/json/get/coOCgLmSqa?callback=JSON_CALL")
+    $http.jsonp("http://www.json-generator.com/api/json/get/cflPmSUZMy?callback=JSON_CALLBACK")
       .then(function success(data){
         console.log ("todo bien")
         console.log(data)

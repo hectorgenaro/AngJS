5293b514bafe260261cd6bc0f9d2ac2daf422f03
hectorgenaro <hectorgenaro@gmail.com>
2020-01-21 00:22:11 -0600

    add ejercicio watchers

--- watchers/angular/controladores.js
+++ watchers/angular/controladores.js
@@ -1,12 +1,10 @@
 var app = angular.module('paginacionApp.controladores',[]);
 
 app.controller('observadorCtrl', ['$scope', function ($scope) {
-	
-	
 
-
-
-
-	
+	$scope.nombre = "Hector";
+	$scope.$watch('nombre', function(valorNuevo, valorViejo){
+		console.log(valorNuevo, valorViejo)
+	})
 
 }]);

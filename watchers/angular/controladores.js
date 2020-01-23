e07bae1eaa1acf52f84f083bb498e61ae4a5fd19
hectorgenaro <hectorgenaro@gmail.com>
2020-01-22 23:51:08 -0600

    add cambio de variable

--- watchers/angular/controladores.js
+++ watchers/angular/controladores.js
@@ -2,7 +2,7 @@ var app = angular.module('paginacionApp.controladores',[]);
 
 app.controller('observadorCtrl', ['$scope', function ($scope) {
 
-	$scope.nombre = "Hector";
+	$scope.nombre = "Hector Rodriguez";
 	$scope.$watch('nombre', function(valorNuevo, valorViejo){
 		console.log(valorNuevo, valorViejo)
 	})

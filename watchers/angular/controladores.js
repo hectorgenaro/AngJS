e871b2d6ffaca045db38301d7e26eeb2532f4c8d
hectorgenaro <hectorgenaro@gmail.com>
2020-01-27 00:55:26 -0600

    add campo curp validacion 16 caracteres

--- watchers/angular/controladores.js
+++ watchers/angular/controladores.js
@@ -1,17 +1,29 @@
-var app = angular.module('paginacionApp.controladores',[]);
+var app = angular.module('paginacionApp.controladores', []);
 
-app.controller('observadorCtrl', ['$scope', function ($scope) {
+app.controller('observadorCtrl', ['$scope', function($scope) {
 
-	// $scope.nombre = "Hector Rodriguez";
-	$scope.nomInco = false;
-	$scope.$watch('nombre', function(valorNuevo, valorViejo){
-		if (!valorNuevo) return;
-		if (valorNuevo.length < 5){
-			$scope.nomIncop = true;
-		} else {
-			$scope.nomIncop = false;
-		}
-		// console.log(valorNuevo, valorViejo)
-	})
+  // $scope.nombre = "Hector Rodriguez";
+  $scope.nomInco = false;
+
+  $scope.$watch('nombre', function(valorNuevo, valorViejo) {
+    if (!valorNuevo) return;
+    if (valorNuevo.length < 5) {
+      $scope.nomIncop = true;
+    } else {
+      $scope.nomIncop = false;
+    }
+    // console.log(valorNuevo, valorViejo)
+  })
+
+  $scope.curp_valid = false;
+  $scope.$watch('curp', function(nuevo, anterior) {
+    if (!nuevo) return;
+    if (nuevo.length < 16) {
+      $scope.curp_valid = true;
+    } else {
+      $scope.curp_valid = false;
+    }
+  })
+
 
 }]);
